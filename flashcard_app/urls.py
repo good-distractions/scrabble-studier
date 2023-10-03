ec4dc5b8ac8892df52ae841c226c9e4edb929a6c
mnwana <mariellenwana@gmail.com>
2023-10-02 23:05:56 -0400

    add page for learning words

--- flashcard_app/urls.py
+++ flashcard_app/urls.py
@@ -6,8 +6,10 @@ app_name = 'flashcard_app'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('dictionary/<int:pk>', views.DictionaryDetailView.as_view(), name='dictionary'),
+    path('dictionary/learn/<int:pk>', views.DictionaryStudyView.as_view(), name='study_dictionary'),
     path('dictionaries/', views.DictionaryListView.as_view(), name='dictionaries'),
     path('add_dictionary/', views.DictionaryCreateView.as_view(),
          name='add_dictionary'),
     path('delete_dictionary/<int:pk>', views.DictionaryDeleteView.as_view(), name='delete_dictionary'),
+    
 ]
--- flashcard_app/views.py
+++ flashcard_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, FormView, CreateView, DetailView, ListView, UpdateView, DeleteView
+from django.views.generic import TemplateView, CreateView, DetailView, ListView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 from urllib import request
 from flashcard_app import models
@@ -42,7 +42,7 @@ class DictionaryDetailView(DetailView):
         # print(data)
         data.columns=['words']
         print(data)
-        context['data'] = data
+        context['preview_data'] = data
         return context
 
 
@@ -51,3 +51,19 @@ class DictionaryListView(ListView):
     fields = '__all__'
     context_object_name = 'dictionary_list'
     
+
+class DictionaryStudyView(TemplateView):
+    template_name = 'flashcard_app/dictionary_study_view.html'
+    # similar to supclub categories, use kwarg to determine filter for annagrams, 
+    # words without vowels etc
+    
+    def get_context_data(self, **kwargs):
+        context = super(DictionaryStudyView, self).get_context_data(**kwargs)
+        primary_key = super(DictionaryStudyView, self).get_context_data(**kwargs)['pk']
+        dictionary = models.Dictionary.objects.get(pk=primary_key)
+        context['dictionary'] = dictionary
+        data = pd.read_csv(dictionary.file, header=None)
+        data = data.iloc[:, 0].head(10)
+        print(data)
+        print(context)
+        return context
