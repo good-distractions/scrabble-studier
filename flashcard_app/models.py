d43000c79588b786682242c611abf84f4bea3e21
mnwana <mariellenwana@gmail.com>
2023-10-06 16:13:12 -0400

    add file format validator

--- flashcard_app/models.py
+++ flashcard_app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.urls import reverse
 import pathlib
+from django.core.validators import FileExtensionValidator
+
 
 class Dictionary(models.Model):
     title = models.CharField(max_length=60)
     source = models.URLField( max_length=400,blank=True, default=True,null=True)
-    file = models.FileField(blank=False, null=False)
+    file = models.FileField(blank=False, null=False,validators=[FileExtensionValidator(allowed_extensions=["csv"])])
