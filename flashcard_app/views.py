e0dc630600af4fb6b6633e29a9c7da8daba8a37c
mnwana <mariellenwana@gmail.com>
2023-10-06 00:13:18 -0400

    fix typos

--- flashcard_app/views.py
+++ flashcard_app/views.py
@@ -1,5 +1,6 @@
+from typing import Any
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, DetailView, ListView, UpdateView, DeleteView
+from django.views.generic import TemplateView, CreateView, DetailView, ListView, UpdateView, DeleteView,FormView
 from django.urls import reverse, reverse_lazy
 from urllib import request
 from flashcard_app import models
@@ -7,7 +8,9 @@ import pandas as pd
 from django.utils.html import format_html
 import json
 from django.http import HttpResponse
-from forms import CustomFilterForm
+from .forms import CustomFilterForm
+from django.views.generic.edit import FormMixin, SingleObjectMixin
+
 
 
 class HomeView(TemplateView):
@@ -17,39 +20,114 @@ class HomeView(TemplateView):
 class DictionaryCreateView(CreateView):
     model = models.Dictionary
     fields = '__all__'
-    success_url = reverse_lazy('portfolio_app:dictionaries')
+    success_url = reverse_lazy('flashcard_app:dictionaries')
 
 
 class DictionaryUpdateView(UpdateView):
     model = models.Dictionary
     fields = '__all__'
     # change to take you to dictionary detail
-    success_url = reverse_lazy('portfolio_app:dictionaries')
+    success_url = reverse_lazy('flashcard_app:dictionaries')
 
 
 class DictionaryDeleteView(DeleteView):
     model = models.Dictionary
     fields = '__all__'
-    success_url = reverse_lazy('portfolio_app:dictionaries')
+    success_url = reverse_lazy('flashcard_app:dictionaries')
 
 
-class DictionaryDetailView(DetailView,FormMixin):
+# class DictionaryDetailView(Form,FormMixin):
+#     model = models.Dictionary
+#     fields = '__all__'
+#     form_class = CustomFilterForm
+#     success_url = reverse_lazy('flashcard_app:study_dictionary', args=[2])
+    
+#     def get_context_data(self, **kwargs):
+#         context = super(DictionaryDetailView, self).get_context_data(**kwargs)
+#         self.object = self.get_object()
+#         data = pd.read_csv(self.object.file, header=None)
+#         data = data.iloc[:, 0].to_frame().head(5)
+#         data.columns = ['words']
+#         context['preview_data'] = data
+#         # print(context)
+#         # print(self.get_success_url())
+#         return context
+
+class DictionaryDetailView(DetailView, FormMixin):
     model = models.Dictionary
     fields = '__all__'
-    
+    # https://stackoverflow.com/questions/45659986/django-implementing-a-form-within-a-generic-detailview
+    form_class = CustomFilterForm
+
+    def get_success_url(self):
+        form = self.form
+        filter_type = self.form['filter_type']
+        substring = self.form['substring']
+        word_length = self.form['word_length']
+        print(form)
+        # return reverse_lazy('flashcard_app:study_dictionary', args=(self.object.id,))
+        return reverse_lazy('flashcard_app:study_dictionary', args=(self.object.id,filter_type,substring,word_length))
+
     def get_context_data(self, **kwargs):
         context = super(DictionaryDetailView, self).get_context_data(**kwargs)
-        context['filter_form'] = CustomFilterForm
-        self.object = self.get_object()
-        data = pd.read_csv(self.object.file, header=None)
-        # print(data)
-        data = data.iloc[:, 0].to_frame().head(5)
-        # print(data)
-        data.columns = ['words']
-        # print(data)
-        context['preview_data'] = data
+        context['form'] = CustomFilterForm()
+        # print('context')
         return context
 
+    def post(self, request, *args, **kwargs):
+        # print('post')
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid() and request.POST.get("form_type"):
+            # print('valid')
+            return self.form_valid(form)
+        else:
+            # print('invalid')
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        self.form = form.cleaned_data
+        return super(DictionaryDetailView, self).form_valid(form)
+    # https://stackoverflow.com/questions/47623088/detailview-object-relations    
+
+# # class DictionaryDetailView(FormView):
+#     form_class = CustomFilterForm
+#     template_name = 'flashcard_app/dictionary_detail.html'
+#     # success_url = reverse_lazy('flashcard_app:dictionaries', 2)
+    
+#     # def get_form_kwargs(self):
+#     #     kwargs = super(DictionaryDetailView,self).get_form_kwargs()
+#     #     # kwargs['request'] = self.request
+#     #     print(kwargs)
+#     #     return kwargs
+    
+#     def get_context_data(self, **kwargs):
+#         context = super(DictionaryDetailView,self).get_context_data(**kwargs)
+#         print(context)
+#         context['dictionary'] = self.get_object_or_404(models.Dictionary, self.kwargs['pk'])
+#         return super(DictionaryDetailView,self).get_context_data(**kwargs)
+    
+#     def form_valid(self,form):
+#         pass
+#         return super(DictionaryDetailView, self).form_valid(form)
+    
+#     def get_success_url(self, **kwargs):
+#         context = super(DictionaryDetailView,self).get_context_data(**kwargs)
+#         form_data = self.form
+#         context['form_data'] = form_data
+#         primary_key = context['dictionary'].pk
+#         return reverse_lazy('flashcard_app:study_dictionary',args=[primary_key])
+#         # return reverse_lazy('flashcard_app:study_dictionary',
+#         #                     args = [self.object.pk,
+#         #                             form_data['filter_type'],
+#         #                             form_data['substring'],
+#         #                             form_data['word_length']])
+#         # return reverse_lazy('flashcard_app:study_dictionary',
+#         #             kwargs = {'pk':self.object.pk,
+#         #                 'filter_type':form_data['filter_type'],
+#         #                 'word_length':form_data['word_length'],
+#         #                 'substring':form_data['substring']})
+    
 
 class DictionaryListView(ListView):
     model = models.Dictionary
