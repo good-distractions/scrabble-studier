b7d7c8183d67f41b402a8778589d138f8d04f95c
Marielle Nwana <mariellenwana@gmail.com>
2023-10-13 01:54:16 -0400

    Update settings.py

--- flashcard_site/settings.py
+++ flashcard_site/settings.py
@@ -156,7 +156,7 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 # STATIC_URL = AWS_URL + '/static/'
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = os.getenv('STATICFILES_STORAGE')
 
 STATIC_URL = os.getenv('STATIC_URL')
 STATICFILES_DIRS = [
