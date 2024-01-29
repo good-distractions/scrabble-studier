7f4e0bbd17f5a21bcc6c21540da6e778a39e8831
mnwana <mariellenwana@gmail.com>
2024-01-29 15:51:30 -0500

    update cors allow setting

--- flashcard_site/settings.py
+++ flashcard_site/settings.py
@@ -199,6 +199,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = ['*']
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = os.getenv('ORIGINS').split(' ')
 CSRF_TRUSTED_ORIGINS = os.getenv('ORIGINS').split(' ')
