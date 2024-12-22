4275398960493f006b5ead1d08a73182975b1c86
mnwana <mariellenwana@gmail.com>
2024-12-22 01:54:50 -0500

    try to fix static issues in prod

--- flashcard_site/settings.py
+++ flashcard_site/settings.py
@@ -179,7 +179,7 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = "/static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 MEDIA_URL = AWS_URL + '/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
