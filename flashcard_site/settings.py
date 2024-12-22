424600f47cbda2e851e02f2e2205c0e71abe732b
mnwana <mariellenwana@gmail.com>
2024-12-22 01:52:30 -0500

    try to fix static issues in prod

--- flashcard_site/settings.py
+++ flashcard_site/settings.py
@@ -177,14 +177,10 @@ AWS_S3_REGION_NAME = 'us-east-2'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 
-# Static files (CSS, JavaScript, Images)
-# STATICFILES_STORAGE = os.getenv('storages.backends.s3boto3.S3Boto3Storage')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = AWS_URL + '/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
